9c3593d275087d039b3101ccfe4a35badb2145b5
gunyu1019 <gunyu1019@gmail.com>
2025-03-06 01:27:25 +0000

    [Feat] Implement Shape Controller Handler

--- ios/Classes/controller/overlay/handler/ShapeControllerHandler.swift
+++ ios/Classes/controller/overlay/handler/ShapeControllerHandler.swift
@@ -4,13 +4,13 @@ import Flutter
 internal protocol ShapeControllerHandler {
     var shapeManager: ShapeManager { get };
     
-    func createShapeLabelLayer(layerId: String, zOrder: Int, passType: ShapeLayerPassType, onSuccess: (Any?) -> Void)
+    func createShapeLayer(layerId: String, zOrder: Int, passType: ShapeLayerPassType, onSuccess: (Any?) -> Void)
 
-    func removeShapeLabelLayer(layerId: String, onSuccess: (Any?) -> Void)
+    func removeShapeLayer(layerId: String, onSuccess: (Any?) -> Void)
 
-    func addPolygonStyle(style: PolygonStyleSet, onSuccess: (String) -> Void)
+    func addPolygonShapeStyle(style: PolygonStyleSet, onSuccess: (String) -> Void)
 
-    func addPolylineStyle(style: PolylineStyleSet, onSuccess: (String) -> Void)
+    func addPolylineShapeStyle(style: PolylineStyleSet, onSuccess: (String) -> Void)
 
     func addMapPolygonShape(option: MapPolygonShapeOptions, onSuccess: (String) -> Void)
 
@@ -48,6 +48,37 @@ internal extension ShapeControllerHandler {
         let shape = mapPolylineShape ?? mapPolygonShape ?? polylineShape ?? polygonShape
 
         switch call.method {
+        case "createShapeLayer":
+            let zOrder = castSafty(payload["zOrder"], caster: asInt) ?? 10001
+            let passType = castSafty(payload["passType"], caster: { ShapeLayerPassType(rawValue: asInt($0))! }) ?? ShapeLayerPassType.default
+            createShapeLayer(layerId: layerId, zOrder: zOrder, passType: passType, onSuccess: result)
+        case "removeShapeLayer": removeShapeLayer(layerId: layerId, onSuccess: result)
+        case "addPolylineShapeStyle": addPolylineShapeStyle(PolylineStyleSet(payload: payload), onSuccess: result)
+        case "addPolygonShapeStyle": addPolygonShapeStyle(PolygonStyleSet(payload: payload), onSuccess: result)
+        case "addPolylineShape":
+            let polyline = asDict(payload["polyline"]!)
+            let position = asDict(polyline["position"]!)
+            let positionType = asInt(position["type"]!)
+            if (positionType == 0) {
+                let option = MapPolylineShapeOptions(payload: polyline)
+                addMapPolylineShape(option: option, onSuccess: result)
+            } else if (positionType == 1) {
+                let option = PolylineShapeOptions(payload: polyline)
+                addPolylineShape(option: option, onSuccess: result)
+            } else 
+                result(FlutterMethodNotImplemented)
+        case "addPolygonShape":
+            let polygon = asDict(payload["polygon"]!)
+            let position = asDict(polygon["position"]!)
+            let positionType = asInt(position["type"]!)
+            if (positionType == 0) {
+                let option = MapPolygonShapeOptions(payload: polyline)
+                addMapPolygonShape(option: option, onSuccess: result)
+            } else if (positionType == 1) {
+                let option = PolygonShapeOptions(payload: polyline)
+                addPolygonShape(option: option, onSuccess: result)
+            } else 
+                result(FlutterMethodNotImplemented)
         default: result(FlutterMethodNotImplemented)
         }
     }
