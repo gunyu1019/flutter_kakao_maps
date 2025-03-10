1af50a5a102b6836febc004ff58e9af6f0b65149
gunyu1019 <gunyu1019@gmail.com>
2025-03-10 03:57:50 -0700

    [Fix] Define types to fix compiler

--- ios/Classes/converter/RouteTypeConverter.swift
+++ ios/Classes/converter/RouteTypeConverter.swift
@@ -4,10 +4,10 @@ import KakaoMapsSDK
 extension RoutePattern {
     convenience init(payload: [String: Any]) {
         self.init(
-            pattern: UIImage(payload: asDict(payload["patternImage"])),
-            distance: asFloat(payload["distance"]),
-            symbol: castSafty(payload["symbolImage"], caster: { 
-                UIImage(asDict($0))
+            pattern: asImage(payload: asDict(payload["patternImage"]!))!,
+            distance: asFloat(payload["distance"]!),
+            symbol: castSafty(payload["symbolImage"], caster: {
+                asImage(payload: asDict($0))!
             }),
             pinStart: castSafty(payload["pinEnd"], caster: asBool) ?? false,
             pinEnd: castSafty(payload["pinEnd"], caster: asBool) ?? false
@@ -20,17 +20,17 @@ extension PerLevelRouteStyle {
         if payload["strokeSize"] == nil || payload["strokeColor"] == nil {
             self.init(
                 width: asUInt(payload["lineWidth"]!), 
-                color: UIColor(value: asUInt(payload["color"])),
+                color: UIColor(value: asUInt(payload["color"]!)),
                 strokeWidth: asUInt(payload["strokeSize"]!),
                 strokeColor: UIColor(value: asUInt(payload["strokeColor"]!)),
-                level: castSafty(payload["zoomLevel"], caster=asInt) ?? 0, 
+                level: castSafty(payload["zoomLevel"], caster: asInt) ?? 0,
                 patternIndex: patternIndex
             )
         } else {
             self.init(
                 width: asUInt(payload["lineWidth"]!), 
-                color: UIColor(value: asUInt(payload["color"])),
-                level: castSafty(payload["zoomLevel"], caster=asInt) ?? 0, 
+                color: UIColor(value: asUInt(payload["color"]!)),
+                level: castSafty(payload["zoomLevel"], caster: asInt) ?? 0,
                 patternIndex: patternIndex
             )
         }
@@ -42,20 +42,20 @@ extension RouteStyleSet {
     convenience init(payload: [String: Any]) {
         let styleId = castSafty(payload["styleId"], caster: asString) ?? UUID().uuidString
         var patterns = [RoutePattern]()
-        let styleSets = castSafty(payload["styles"], caster: {
-            asArray($0, caster: {
-                let rawStyles = asDict($0)
+        let styleSets: [RouteStyle] = castSafty(payload["styles"], caster: {
+            asArray($0, caster: { (styleSetElement: Any) -> RouteStyle in
+                let rawStyles = asDict(styleSetElement)
                 var patternIndex = -1
                 if rawStyles["pattern"] != nil && rawStyles["pattern"] is NSNull {
                     patterns.append(
-                        RoutePattern(payload: asDict(rawStyles["pattern"]!), patternIndex: patternIndex)
+                        RoutePattern(payload: asDict(rawStyles["pattern"]!))
                     )
                     patternIndex = patterns.count - 1
                 }
                 var styles = [PerLevelRouteStyle]()
-                styles.append(PerLevelRouteStyle(payload: rawStyles))
+                styles.append(PerLevelRouteStyle(payload: rawStyles, patternIndex: patternIndex))
                 styles.append(
-                    contentsOf: asArray(rawStyles["otherStyle"] ?? [], caster: asDict).map {
+                    contentsOf: asArray(rawStyles["otherStyle"] ?? [], caster: asDict).map { (styleElement) -> PerLevelRouteStyle in
                         patternIndex = -1
                         if rawStyles["pattern"] != nil && rawStyles["pattern"] is NSNull {
                             patterns.append(
@@ -63,11 +63,11 @@ extension RouteStyleSet {
                             )
                             patternIndex = patterns.count - 1
                         }
-                        PerLevelRouteStyle(payload: $0, patternIndex: patternIndex)   
+                        return PerLevelRouteStyle(payload: styleElement, patternIndex: patternIndex)
                     }
                 )
 
-                RouteStyle(styles: styles)
+                return RouteStyle(styles: styles)
             })
         }) ?? []
 
