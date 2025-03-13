e9c0e8cfb5e7f070a9776335fd7e64ee121027f7
gunyu1019 <gunyu1019@gmail.com>
2025-03-13 11:29:58 +0000

    [Docs] Docstring RoutePattern

--- lib/models/route/route_pattern.dart
+++ lib/models/route/route_pattern.dart
@@ -1,11 +1,20 @@
 part of '../../kakao_map_sdk.dart';
 
+/// [RouteStyle]에서 사용되며 스타일에 패턴(규칙)을 정의하는 객체입니다.
 class RoutePattern with KMessageable {
+  /// 패턴에 사용할 이미지입니다.
   final KImage patternImage;
+  
+  /// 패턴 이미지 외에 패턴의 속성을 표시할 심볼 이미지입니다.
   final KImage? symbolImage;
+
+  /// 패턴 이미지를 표시하는 간격입니다.
   final double distance;
 
+  /// 패턴의 시작 지점에 이미지를 고정하여 그릴지 정의합니다.
   bool pinStart;
+
+  /// 패턴의 끝 지점에 이미지를 고정하여 그릴지 정의합니다.
   bool pinEnd;
 
   RoutePattern(
