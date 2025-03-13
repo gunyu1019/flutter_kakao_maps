e4a04ce8f6fe860177071ce55d281410ce9a8e53
gunyu1019 <gunyu1019@gmail.com>
2025-03-13 14:19:29 +0000

    [Docs] Docstring Base Dot Point.

--- lib/models/shape/base_dot_point.dart
+++ lib/models/shape/base_dot_point.dart
@@ -1,17 +1,25 @@
 part of '../../kakao_map_sdk.dart';
 
+/// [Polygon] 또는 [Polygon]의 도형을 상대 좌표으로 구성할 때 사용하는 객체입니다.
+/// 도형을 [basePoint]을 기준으로 좌표를 기준으로 도형을 생성합니다.
 sealed class _BaseDotPoint extends BasePoint {
+  /// 도형을 구성하는 기준 지점을 정의합니다. 도형을 구성할 때 필수로 필요한 값입니다.
+  /// [addHole]을 이용하여 구멍을 구성하는 경우, [basePoint]를 정의할 필요는 없습니다.
   final LatLng? basePoint;
   final List<_BaseDotPoint> _holes = [];
 
   _BaseDotPoint(this.basePoint);
 
+  /// 도형에 구멍을 추가합니다.
   void addHole(_BaseDotPoint hole) => _holes.add(hole);
 
+  /// 도형에 구성한 구멍의 개수를 불러옵니다.
   int get holeCount => _holes.length;
 
+  /// [_BaseDotPoint.addHole] 함수로 추가한 구멍을 [index] 순서에 따라 해당하는 객체를 불러옵니다.
   _BaseDotPoint? getHole(int index) => _holes[index];
 
+  /// [_BaseDotPoint.addHole] 함수로 추가한 구멍을 [index] 순서에 따라 해당하는 객체를 삭제합니다.
   void removeHole(int index) => _holes.removeAt(index);
 
   @override
