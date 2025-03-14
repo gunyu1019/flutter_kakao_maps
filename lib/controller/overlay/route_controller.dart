75fc1ef801a9bcdaedc43b0520fd87b137cc73e4
gunyu1019 <gunyu1019@gmail.com>
2025-03-14 11:30:01 +0000

    [Docs] Docstring route controller.

--- lib/controller/overlay/route_controller.dart
+++ lib/controller/overlay/route_controller.dart
@@ -1,5 +1,6 @@
 part of '../../kakao_map_sdk.dart';
 
+/// 지도에 [Route] 또는 [MultipleRoute]를 생성하거나 삭제하는 등의 개체 관리를 할 수 있는 컨트롤러입니다.
 class RouteController extends OverlayController {
   @override
   MethodChannel channel;
@@ -10,7 +11,10 @@ class RouteController extends OverlayController {
   @override
   OverlayType get type => OverlayType.route;
 
+  /// [RouteController]의 고유 ID입니다.
   final String id;
+
+  /// 렌더링의 우선순위를 정의합니다.
   final int zOrder;
 
   final Map<String, Route> _route = {};
@@ -56,6 +60,9 @@ class RouteController extends OverlayController {
     return super._invokeMethod(method, payload);
   }
 
+  /// 지도에 새로운 단일 선형([Route])을 그립니다.
+  /// [Route]를 그리기 위해서는 지점([points])과 스타일([style])이 필수로 입력되어야 합니다.
+  /// Route에서 사용되는 [id]는 이미 등록된 [MultipleRoute.id]와 중복될 수 없습니다.
   Future<Route> addRoute(List<LatLng> points, RouteStyle style,
       {String? id, CurveType curveType = CurveType.none, int zOrder = 10000}) async {
     final styleId = style.id ?? await manager.addRouteStyle(style);
@@ -75,6 +82,8 @@ class RouteController extends OverlayController {
     return route;
   }
 
+  /// 지도에 새로운 다중 선형([MultipleRoute])을 그립니다.
+  /// [MultipleRouteOption.id]는 이미 등록된 [Route]의 ID와 중복될 수 없습니다.
   Future<MultipleRoute> addMultipleRoute(MultipleRouteOption option) async {
     if (!option._isStyleAdded()) {
       await manager.addMultipleRouteStyle(option._styles);
@@ -91,15 +100,19 @@ class RouteController extends OverlayController {
     return route;
   }
 
+  /// 입력된 [id]에 따라 지도에 그려진 [Route]를 불러옵니다.
   Route? getRoute(String id) => _route[id];
 
+  /// 입력된 [id]에 따라 지도에 그려진 [MultipleRoute]를 불러옵니다.
   MultipleRoute? getMultipleRoute(String id) => _multipleRoute[id];
 
+  /// 입력된 [route]에 따라 지도에 그려진 [Route]를 삭제합니다.
   Future<void> removeRoute(Route route) async {
     await _invokeMethod("removeRoute", {"routeId": route.id});
     _route.remove(route.id);
   }
 
+  /// 입력된 [route]에 따라 지도에 그려진 [MultipleRoute]를 삭제합니다.
   Future<void> removeMultipleRoute(MultipleRoute route) async {
     await _invokeMethod("removeRoute", {"routeId": route.id});
     _multipleRoute.remove(route.id);
