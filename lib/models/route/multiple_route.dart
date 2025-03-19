c672a90c099120301714358731929c0a5647986e
gunyu1019 <gunyu1019@gmail.com>
2025-03-19 14:22:58 +0000

    [Feat] Improve structure in MultipleRoute.

--- lib/models/route/multiple_route.dart
+++ lib/models/route/multiple_route.dart
@@ -2,7 +2,10 @@ part of '../../kakao_map_sdk.dart';
 
 /// 지도에 다중 선형([MultipleRoute])를 나타내는 객체입니다.
 /// [MultipleRoute]는 지도에 선형의 경로(길찾기 라인)를 다양하게 표현합니다.
-class MultipleRoute extends BaseRoute {
+class MultipleRoute extends BaseRoute with BaseMultipleRoute {
+  final List<RouteSegment> _segments;
+  List<RouteStyle> _styles;
+
   @override
   final RouteController _controller;
 
@@ -15,54 +18,27 @@ class MultipleRoute extends BaseRoute {
   @override
   int _zOrder;
 
-  /// [MultipleRoute]에 적용된 곡선 유형입니다.
-  final List<CurveType> _curveType;
+  @override
+  List<RouteStyle> get styles => _styles;
 
-  final List<List<LatLng>> _points;
-  final List<int> _styleIndex;
-  List<RouteStyle> _styles;
+  @override
+  List<RouteSegment> get segments => _segments;
 
   MultipleRoute._(this._controller, this.id,
-      {required List<List<LatLng>> points,
-      required List<RouteStyle> style,
-      required List<CurveType> curveType,
-      required List<int> styleIndex,
-      required int zOrder})
-      : _points = points,
-        _styles = style,
-        _curveType = curveType,
-        _styleIndex = styleIndex,
+      {required List<RouteStyle> styles,
+      required List<RouteSegment> segments,
+      required int zOrder}) :
+        _segments = segments,
+        _styles = styles,
         _visible = true,
-        _zOrder = zOrder;
-
-  /// [index]에 따라 단일 선형([Route]) 형태로 불러옵니다.
-  /// 불러온 [Route] 객체에서 [Route.changeStyle], [Route.changePoint] 등의 수정을 할 수는 없습니다.
-  Route getRoute(int index) => Route._fromMultiple(_controller, id, this,
-      points: _points[index],
-      style: _styles[_styleIndex[index]],
-      curveType: _curveType[index]);
-
-  /// [MultipleRoute]에 정의된 스타일을 [index] 순에 따라 [RouteStyle] 형태로 불러옵니다.
-  RouteStyle getStyle(int index) => _styles[_styleIndex[index]];
-
-  /// [MultipleRoute]에 정의된 스타일을 [index] 순에 따라 정의된 선형의 지점을 불러옵니다.
-  List<LatLng> getPoints(int index) => _points[index];
-
-  /// [MultipleRoute]에 정의된 스타일을 [index] 순에 따라 정의된 선형의 곡선 유형([CurveType])을 불러옵니다.
-  CurveType getCurveType(int index) => _curveType[index];
-
-  /// [index]에 따라 정의된 선형의 지점([points])을 다시 정의합니다.
-  Future<void> changePoint(int index, List<LatLng> points) async {
-    _points[index] = points;
-    await _controller._changeRoute(id, _styles[0].id!, _curveType, _points);
-  }
-
-  /// [index]에 따라 정의된 선형의 곡선 유형([curveType])을 다시 정의합니다.
-  Future<void> changeCurveType(int index, CurveType curveType) async {
-    _curveType[index] = curveType;
-    await _controller._changeRoute(id, _styles[0].id!, _curveType, _points);
-  }
-
+        _zOrder = zOrder {
+          for(RouteSegment segment in _segments) {
+            segment.id = id;
+            segment.parent = this;
+            segment._controller = _controller;
+          }
+        }
+    
   /// [MultipleRoute]에서 사용하는 스타일([RouteStyle])을 다시 정의합니다.
   Future<void> changeStyle(List<RouteStyle> styles) async {
     if (styles.isEmpty) {
@@ -70,7 +46,7 @@ class MultipleRoute extends BaseRoute {
     }
     final styleId =
         styles[0].id ?? await _controller.manager.addMultipleRouteStyle(styles);
-    await _controller._changeRoute(id, styleId, _curveType, _points);
+    await _controller._changeMultipleRoute(id, styleId, segments);
     _styles = styles;
   }
 }
