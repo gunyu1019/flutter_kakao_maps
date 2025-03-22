cda02b95530fe8da67ae4ad0dc6f102b3aba479e
gunyu1019 <gunyu1019@gmail.com>
2025-03-22 01:30:00 +0000

    [Feat] Divide poi interaction event and lod poi interaction event.

--- ios/Classes/listener/PoiClickListener.swift
+++ ios/Classes/listener/PoiClickListener.swift
@@ -3,28 +3,26 @@ import KakaoMapsSDK
 
 class PoiClickListener {
     private let channel: FlutterMethodChannel
+    var enable: Bool
 
     init(channel: FlutterMethodChannel) {
         self.channel = channel
+        self.enable = false
     }
 
-    func onPoisInteractionEvent(_ param: PoisInteractionEventParam) {
-        let labelManager = param.kakaoMap.getLabelManager()
-        let layerID = param.layerID
-        let poiId = param.poiID
-
-        let lodLayer = labelManager.getLodLabelLayer(layerID)
-
-        if lodLayer != nil {
-            channel.invokeMethod("onLodPoiClick", arguments: [
-                "layerId": layerID,
-                "poiId": poiId,
-            ])
-            return
-        }
+    func onPoiInteractionEvent(_ param: PoiInteractionEventParam) {
+        if enable return
         channel.invokeMethod("onPoiClick", arguments: [
-            "layerId": layerID,
-            "poiId": poiId,
+            "layerId": param.poiItem.layerID,
+            "poiId": param.poiItem.itemID,
+        ])
+    }
+
+    func onLodPoiInteractionEvent(_ param: PoiInteractionEventParam) {
+        if enable return
+        channel.invokeMethod("onLodPoiClick", arguments: [
+            "layerId": param.poiItem.layerID,
+            "poiId": param.poiItem.itemID,
         ])
     }
 }
