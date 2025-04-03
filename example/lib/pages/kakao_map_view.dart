770c51d03c68a92732c3f75c766f21dca200db16
gunyu1019 <gunyu1019@gmail.com>
2025-04-03 10:52:09 +0000

    [Feat] (Example) Apply other style in kakao map view.

--- example/lib/pages/kakao_map_view.dart
+++ example/lib/pages/kakao_map_view.dart
@@ -27,27 +27,35 @@ class _KakaoMapViewState extends State<KakaoMapView> {
   @override
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
-    var children = <Widget>[
-      SizedBox(
-        width: mediaQuery.size.width,
-        height: mediaQuery.size.height,
-        child: mapWidget(context), 
-      )
-    ];
+    var children = <Widget>[];
     if (mediaQuery.size.height > mediaQuery.size.width) {
       // Apply Mobile Position
       children.add(
+        SizedBox(
+          width: mediaQuery.size.width,
+          height: mediaQuery.size.height * 0.7,
+          child: mapWidget(context), 
+        )
+      );
+      children.add(
         Positioned(
-          bottom: 10,
+          bottom: 0,
           child: SizedBox(
             width: mediaQuery.size.width - 20,
-            height: 300,
+            height: mediaQuery.size.height * 0.3,
             child: const KakaoMapControllerWidget(),
           ),
         )
       );
     } else { 
       // Apply Tablet/PC Position
+      children.add(
+        SizedBox(
+          width: mediaQuery.size.width,
+          height: mediaQuery.size.height,
+          child: mapWidget(context), 
+        )
+      );
       children.add(
         const Positioned(
           top: 10,
