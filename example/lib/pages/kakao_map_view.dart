450596e5df426db28fdee7c6f98c1b275d6b33d8
gunyu1019 <gunyu1019@gmail.com>
2025-04-03 14:25:22 +0000

    [Feat] (Example) Implement mobile first.

--- example/lib/pages/kakao_map_view.dart
+++ example/lib/pages/kakao_map_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:kakao_map_sdk/kakao_map_sdk.dart';
 import 'package:kakao_map_sdk_example/components/controller_mobile.dart';
@@ -9,7 +8,6 @@ class KakaoMapView extends StatefulWidget {
   @override
   State<KakaoMapView> createState() => _KakaoMapViewState();
 }
-
 
 class _KakaoMapViewState extends State<KakaoMapView> {
   late KakaoMapController controller;
@@ -28,49 +26,23 @@ class _KakaoMapViewState extends State<KakaoMapView> {
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
     var children = <Widget>[];
-    if (mediaQuery.size.height > mediaQuery.size.width) {
-      // Apply Mobile Position
-      children.add(
-        SizedBox(
-          width: mediaQuery.size.width,
-          height: mediaQuery.size.height * 0.7,
-          child: mapWidget(context), 
-        )
-      );
-      children.add(
-        Positioned(
+
+    children.addAll([
+      AnimatedPositioned(
+          duration: const Duration(milliseconds: 150),
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 200,
+          child: mapWidget(context)),
+      const AnimatedPositioned(
+          duration: Duration(milliseconds: 150),
+          left: 0,
+          right: 0,
           bottom: 0,
-          child: SizedBox(
-            width: mediaQuery.size.width - 20,
-            height: mediaQuery.size.height * 0.3,
-            child: const KakaoMapControllerWidget(),
-          ),
-        )
-      );
-    } else { 
-      // Apply Tablet/PC Position
-      children.add(
-        SizedBox(
-          width: mediaQuery.size.width,
-          height: mediaQuery.size.height,
-          child: mapWidget(context), 
-        )
-      );
-      children.add(
-        const Positioned(
-          top: 10,
-          right: 10,
-          child: SizedBox(
-            width: 300,
-            height: 300,
-            child: KakaoMapControllerWidget(),
-          ),
-        )
-      );
-    }
+          child: MobileControllerWidget()),
+    ]);
     return Stack(
-      alignment: AlignmentDirectional.centerStart,
-      children: children
-    );
+        alignment: AlignmentDirectional.centerStart, children: children);
   }
 }
