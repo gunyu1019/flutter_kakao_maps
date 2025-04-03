20f9e489b3de636464d782336d6aa7594e046001
gunyu1019 <gunyu1019@gmail.com>
2025-04-03 10:45:18 +0000

    [Feat] (Example) Add controller mobile.

---
+++ example/lib/components/controller_mobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class KakaoMapControllerWidget extends StatefulWidget {
+  const KakaoMapControllerWidget({super.key});
+
+  @override
+  State<KakaoMapControllerWidget> createState() => _KakaoMapControllerWidgetState();
+}
+
+class _KakaoMapControllerWidgetState extends State<KakaoMapControllerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Card();
+  }
+}
