52bef81c8eaf70735952cdaba7b50163e47ab0ca
gunyu1019 <gunyu1019@gmail.com>
2025-04-08 04:29:16 -0700

    [Fix] Wrong method called in iOS (ShapeLayer)

--- ios/Classes/controller/overlay/handler/ShapeControllerHandler.swift
+++ ios/Classes/controller/overlay/handler/ShapeControllerHandler.swift
@@ -165,7 +165,7 @@ extension ShapeControllerHandler {
             } else {
                 result(FlutterMethodNotImplemented)
             }
-        case "changeVisibleAllPolyline": changePolygonAllVisible(layer: layer!, visible: asBool(arguments!["visible"]!), onSuccess: result)
+        case "changeVisibleAllPolyline": changePolylineAllVisible(layer: layer!, visible: asBool(arguments!["visible"]!), onSuccess: result)
         case "changeVisibleAllPolygon":
             changePolygonAllVisible(layer: layer!, visible: asBool(arguments!["visible"]!), onSuccess: result)
         default: result(FlutterMethodNotImplemented)
