4c3729a43f696e398182dba7db556470d626649b
gunyu1019 <gunyu1019@gmail.com>
2025-04-14 11:06:40 +0000

    [Feat] #13 - Interopation about kakao.maps.Map named WebMapController.

---
+++ lib/web/interoperability/web_map_controller.dart
@@ -0,0 +1,7 @@
+part of '../../kakao_map_sdk.dart';
+
+
+@JS("kakao.maps.Map")
+extension type WebMapController._(JSObject _) implements JSObject {
+  external WebMapController(web.Element element, WebMapOption option);
+}
