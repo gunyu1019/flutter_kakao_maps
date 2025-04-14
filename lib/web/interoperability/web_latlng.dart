4f811b2f9c0af03e7e4322b3203b9b9fd2e2ea7a
gunyu1019 <gunyu1019@gmail.com>
2025-04-14 11:06:01 +0000

    [Feat] #13 - Interopation about kakao.maps.LatLng

---
+++ lib/web/interoperability/web_latlng.dart
@@ -0,0 +1,14 @@
+part of '../../kakao_map_sdk.dart';
+
+@JS("kakao.maps.LatLng")
+extension type WebLatLng._(JSObject _) implements JSObject {
+  external WebLatLng(double latitude, double longitude);
+
+  factory WebLatLng.fromLatLng(LatLng payload) =>
+      WebLatLng(payload.latitude, payload.longitude);
+
+  external double getLat();
+  external double getLng();
+
+  LatLng toLatLng() => LatLng(getLat(), getLng());
+}
