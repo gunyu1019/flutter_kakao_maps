0519aab8ea5c5aa0fe5856172330116458bbc477
gunyu1019 <gunyu1019@gmail.com>
2025-04-18 04:30:49 -0700

    [Fix] Invalid point of convertMapPointToPoint.

--- ios/Classes/utils/MapPointConverter.swift
+++ ios/Classes/utils/MapPointConverter.swift
@@ -1,13 +1,13 @@
 import KakaoMapsSDK
 
 func convertMapPointToPoint(kakaoMap: KakaoMap, position: MapPoint) -> CGPoint {
-    let minPosition = kakaoMap.getPosition(CGPoint(x: 0.0, y: 0.0))
-    let maxPosition = kakaoMap.getPosition(CGPoint(x: 1.0, y: 1.0))
+    let minPosition = kakaoMap.getPosition(CGPoint(x: kakaoMap.viewRect.width, y: kakaoMap.viewRect.height))
+    let maxPosition = kakaoMap.getPosition(CGPoint(x: 0.0, y: 0.0))
 
     let relativeLatitude = maxPosition.wgsCoord.latitude - minPosition.wgsCoord.latitude
     let relativeLongitude = maxPosition.wgsCoord.longitude - minPosition.wgsCoord.longitude
 
-    let x = (position.wgsCoord.latitude - minPosition.wgsCoord.latitude) / relativeLatitude
-    let y = (position.wgsCoord.longitude - minPosition.wgsCoord.longitude) / relativeLongitude
+    let y = (position.wgsCoord.latitude - minPosition.wgsCoord.latitude) / relativeLatitude * kakaoMap.viewRect.height
+    let x = (position.wgsCoord.longitude - minPosition.wgsCoord.longitude) / relativeLongitude * kakaoMap.viewRect.width
     return CGPoint(x: x, y: y)
 }
