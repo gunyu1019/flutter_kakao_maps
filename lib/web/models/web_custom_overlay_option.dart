781370678f98cd4c75c0c9ee0b46f011dc0ea4dc
gunyu1019 <gunyu1019@gmail.com>
2025-04-18 06:24:43 +0000

    [Feat] #13 - Add WebCustomOverlayOption

---
+++ lib/web/models/web_custom_overlay_option.dart
@@ -0,0 +1,14 @@
+part of '../../kakao_map_sdk.dart';
+
+
+extension type WebCustomOverlayOption._(JSObject _) implements JSObject  {
+  external WebCustomOverlayOption({
+    bool clickable,
+    String content,
+    WebMapController map,
+    WebLatLng position,
+    double xAnchor = 0.5,
+    double yAnchor = 0.5,
+    int zIndex = 10001
+  });
+}
