1491b49aa268de1ecb0b1d9fb0c16ac0e60335c1
gunyu1019 <gunyu1019@gmail.com>
2025-04-18 03:25:20 +0000

    [Feat] #13 - Add getController static function.

--- lib/web/web_initializer.dart
+++ lib/web/web_initializer.dart
@@ -1,25 +1,28 @@
 part of '../kakao_map_sdk.dart';
 
-
 class WebInitializer {
+  static String mapElementId(int viewId) => "map_$viewId";
+  static const int maxAttempts = 100;
+  static const int retryTime = 1;
+
   static void initialize() {
     ui_web.platformViewRegistry.registerViewFactory(
-      KakaoMapWebController.VIEW_TYPE,
-      (int viewId, {Object? params}) => web.HTMLDivElement()
-          ..id = 'map_$viewId'
+        KakaoMapWebController.VIEW_TYPE,
+        (int viewId, {Object? params}) => web.HTMLDivElement()
+          ..id = mapElementId(viewId)
           ..style.width = '100%'
-          ..style.height = '100%'
-    );
-    
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      for (int i = 0; i < 100; i ++) {
-        var element = web.document.getElementById("map_0");
-        if (element != null) {
-          WebMapController(element, WebMapOption(center: WebLatLng(37.39479412020964, 127.11116968185037), level: 6));
-          break;
-        }
-        await Future.delayed(const Duration(seconds: 1));
+          ..style.height = '100%');
+  }
+
+  static Future<WebMapController?> getController(
+      int viewId, WebMapOption option) async {
+    for (int i = 0; i < maxAttempts; i++) {
+      var element = web.document.getElementById("map_$viewId");
+      if (element != null) {
+        return WebMapController(element, option);
       }
-    });
+      await Future.delayed(const Duration(seconds: retryTime));
+    }
+    return null;
   }
 }
