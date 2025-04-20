bd59d018067d815669232ae9be6a8b9c8034346f
gunyu1019 <gunyu1019@gmail.com>
2025-04-20 08:34:35 +0000

    [Feat] #13 - Implement text element

---
+++ lib/web/elements/text_element.dart
@@ -0,0 +1,13 @@
+part of '../../kakao_map_sdk.dart';
+
+web.HTMLElement textElement(String text, PoiTextStyle style) => web
+    .HTMLParagraphElement()
+  ..textContent = text
+  ..style.fontStretch = "${style.aspectRatio * 100}%"
+  ..style.letterSpacing = "${style.characterSpace}px"
+  ..style.color = "rgb(${style.color.r}, ${style.color.g} ,${style.color.b})"
+  ..style.fontFamily = style.font
+  ..style.lineHeight = "${style.lineSpace}px"
+  ..style.fontSize = "${style.size}px"
+  ..style.textShadow =
+      "-${style.stroke}px 0px rgb(${style.strokeColor.r}, ${style.strokeColor.g} ,${style.strokeColor.b}) 0px -${style.stroke}px rgb(${style.strokeColor.r}, ${style.strokeColor.g} ,${style.strokeColor.b}) ${style.stroke}px 0px rgb(${style.strokeColor.r}, ${style.strokeColor.g} ,${style.strokeColor.b}) 0px ${style.stroke}px rgb(${style.strokeColor.r}, ${style.strokeColor.g} ,${style.strokeColor.b}) ";
