af615deb011a6c14a5e28283f3232bc993d17e08
gunyu1019 <gunyu1019@gmail.com>
2025-04-26 14:08:38 +0000

    [Feat] #13 - Add WebPolylineOption.

---
+++ lib/web/models/web_polyline_option.dart
@@ -0,0 +1,12 @@
+part of '../../kakao_map_sdk.dart';
+
+extension type WebPolylineOption._(JSObject _) implements JSObject {
+  external WebPolylineOption(
+      {bool endArrow,
+      List<WebLatLng> path,
+      double strokeWeight = 3,
+      String strokeColor,
+      double strokeOpacity = 0.6,
+      String strokeStyle = "solid",
+      int zIndex = 10000});
+}
