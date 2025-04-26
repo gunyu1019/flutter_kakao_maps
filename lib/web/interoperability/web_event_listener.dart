432112654a448815e96ce80be07156b5235fe2bf
gunyu1019 <gunyu1019@gmail.com>
2025-04-26 08:33:47 +0000

    [Feat] #13 - Modify type of lisenter

--- lib/web/interoperability/web_event_listener.dart
+++ lib/web/interoperability/web_event_listener.dart
@@ -1,4 +1,4 @@
 part of '../../kakao_map_sdk.dart';
 
 @JS("kakao.maps.event.addListener")
-external void addEventListener(WebMapController controller, String event, JSAny listener);
+external void addEventListener(WebMapController controller, String event, JSFunction listener);
