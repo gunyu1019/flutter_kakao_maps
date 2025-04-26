b61a77ef30fa0ff141aab4230f335135c99e665f
gunyu1019 <gunyu1019@gmail.com>
2025-04-26 08:32:33 +0000

    [Feat] #13 - Divide get image data parts in imageElement

--- lib/web/elements/image_element.dart
+++ lib/web/elements/image_element.dart
@@ -1,16 +1,8 @@
 part of '../../kakao_map_sdk.dart';
 
-Future<web.HTMLElement> imageElement(KImage image, [void Function()? onClick]) async {
-  final data = switch(image.type) {
-    ImageType.assets => (await rootBundle.load(image._path!)).buffer.asUint8List(),
-    ImageType.file => await File(image._path!).readAsBytes(),
-    ImageType.data => image._data!,
-  };
-  final source = "data:image/png;base64,${base64Encode(data)}";
 
-  return web.HTMLImageElement()
-    ..width = image.width * 3
-    ..height = image.height * 3
+web.HTMLElement imageElement(String source, int width, int height, [void Function()? onClick]) => web.HTMLImageElement()
+    ..width = width * 3
+    ..height = height * 3
     ..onclick = onClick?.toJS
     ..src = source;
-}
