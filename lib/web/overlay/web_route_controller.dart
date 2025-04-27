e78da2bb3d6fbaf50e60ed158aa8ba4e2a73ea85
gunyu1019 <gunyu1019@gmail.com>
2025-04-27 07:18:07 +0000

    [Feat] #13 - Implement addRoute, and other management route method.

--- lib/web/overlay/web_route_controller.dart
+++ lib/web/overlay/web_route_controller.dart
@@ -3,29 +3,87 @@ part of '../../kakao_map_sdk.dart';
 class WebRouteController extends RouteController {
   final WebMapController controller;
 
+  final Map<String, List<WebRoute>> _webRoute = {};
+  final Map<String, int> _currentRouteLevel = {};
+
   WebRouteController._(this.controller, super.channel, super.manager, super.id)
       : super._();
 
   @override
-  Future<void> _createRouteLayer() async {
-  }
+  Future<void> _createRouteLayer() async {}
 
   @override
   Future<void> _removeRouteLayer() async {
+    for (final route in _route.values) {
+      await removeRoute(route);
+    }
   }
 
   @override
   Future<void> _changeMultipleRoute(
-      String routeId, String styleId, List<RouteSegment> segments) async {
-  }
+      String routeId, String styleId, List<RouteSegment> segments) async {}
 
   @override
   Future<void> _changeRoute(String routeId, String styleId, CurveType curveType,
-      List<LatLng> points) async {
-  }
+      List<LatLng> points) async {}
 
   @override
   Future<void> _changeRouteVisible(String routeId, bool visible) async {
+    for (final route in _webRoute[routeId]!) {
+      if (visible) {
+        route.bodyElement.setMap(controller);
+        route.strokeElement?.setMap(controller);
+        route.patternElement?.setMap(controller);
+      }
+      else {
+        route.bodyElement.setMap(null);
+        route.strokeElement?.setMap(null);
+        route.patternElement?.setMap(null);
+      }
+    }
+  }
+
+  static String _getSingleColorCode(double value) =>
+      (value * 255).toInt().toRadixString(16);
+
+  static String getColorCode(Color color) =>
+      "#${_getSingleColorCode(color.r)}${_getSingleColorCode(color.g)}${_getSingleColorCode(color.b)}";
+
+  WebRoute _addRouteElement(RouteStyle style, List<LatLng> points, int zOrder) {
+    final bodyElementOption = WebPolylineOption(
+        path: points.map(WebLatLng.fromLatLng).toList().toJS,
+        strokeWeight: style.lineWidth * .5,
+        strokeColor: getColorCode(style.color),
+        strokeOpacity: 1,
+        zIndex: zOrder);
+    final strokeElementOption = style.strokeWidth > 0
+        ? WebPolylineOption(
+            path: points.map(WebLatLng.fromLatLng).toList().toJS,
+            strokeWeight: style.lineWidth * .5 + style.strokeWidth * .5,
+            strokeColor: getColorCode(style.strokeColor),
+            strokeOpacity: 1,
+            zIndex: zOrder - 1)
+        : null;
+    final patternElementOption = style.pattern == null
+        ? WebPolylineOption(
+            path: points.map(WebLatLng.fromLatLng).toList().toJS,
+            strokeWeight: 1,
+            strokeColor: getColorCode(style.strokeColor),
+            strokeOpacity: 1,
+            strokeStyle: "longdash",
+            zIndex: zOrder + 1)
+        : null;
+
+    final bodyElement = WebPolyline(bodyElementOption);
+    final strokeElement =
+        strokeElementOption != null ? WebPolyline(strokeElementOption) : null;
+    final patternElement =
+        patternElementOption != null ? WebPolyline(patternElementOption) : null;
+    strokeElement?.setMap(controller);
+    bodyElement.setMap(controller);
+    patternElement?.setMap(controller);
+    return WebRoute(bodyElement, strokeElement, patternElement, bodyElementOption,
+          strokeElementOption, patternElementOption);
   }
 
   @override
@@ -36,17 +94,11 @@ class WebRouteController extends RouteController {
     if (id != null && _route.containsKey(id)) {
       throw DuplicatedOverlayException(id);
     }
-    final styleId = style.id ?? await manager.addRouteStyle(style);
-    Map<String, dynamic> payload = {
-      "route": <String, dynamic>{
-        "id": id,
-        "points": points.map((e) => e.toMessageable()).toList(),
-        "styleId": styleId,
-        "curveType": curveType.value,
-        "zOrder": zOrder
-      }
-    };
-    String routeId = await _invokeMethod("addRoute", payload);
+    style.id ?? await manager.addRouteStyle(style);
+
+    String routeId = "route_overlay_${id}_${_route.length}";
+    _webRoute[routeId] = [_addRouteElement(style, points, zOrder)];
+
     final route = Route._(this, routeId,
         points: points, style: style, curveType: curveType, zOrder: zOrder);
     _route[routeId] = route;
@@ -61,8 +113,9 @@ class WebRouteController extends RouteController {
     if (!option._isStyleAdded()) {
       await manager.addMultipleRouteStyle(option.styles);
     }
-    Map<String, dynamic> payload = {"route": option.toMessageable()};
-    String routeId = await _invokeMethod("addMultipleRoute", payload);
+
+    String routeId = "multiple_route_overlay_${id}_${_route.length}";
+
     final route = MultipleRoute._(this, routeId,
         segments: option.segments,
         styles: option.styles,
@@ -70,15 +123,25 @@ class WebRouteController extends RouteController {
     _route[routeId] = route;
     return route;
   }
-  
+
   @override
   Future<void> removeRoute(BaseRoute route) async {
+    await _changeRouteVisible(route.id, false);
     _route.remove(route.id);
+    _webRoute.remove(route.id);
   }
 
   @override
-  Future<void> showAllRoute() async {}
+  Future<void> showAllRoute() async {
+    for (var route in _route.values) {
+      await route.show();
+    }
+  }
 
   @override
-  Future<void> hideAllRoute() async {}
+  Future<void> hideAllRoute() async { 
+    for (var route in _route.values) {
+      await route.hide();
+    }
+  }
 }
