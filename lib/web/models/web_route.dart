4b55222db59b8cf132bd1c7b0b4821829fb9d24a
gunyu1019 <gunyu1019@gmail.com>
2025-04-27 07:06:18 +0000

    [Feat] #13 - Add option field in WebRoute to modify polyline option.

--- lib/web/models/web_route.dart
+++ lib/web/models/web_route.dart
@@ -2,14 +2,24 @@ part of '../../kakao_map_sdk.dart';
 
 class WebRoute {
   final WebPolyline? strokeElement;
-  final WebPolyline? bodyElement;
+  final WebPolyline bodyElement;
   final WebPolyline? patternElement;
 
-  WebRoute(this.strokeElement, this.bodyElement, this.patternElement);
+  final WebPolylineOption? strokeElementOption;
+  final WebPolylineOption bodyElementOption;
+  final WebPolylineOption? patternElementOption;
+
+  WebRoute(
+    this.bodyElement,
+    this.strokeElement,
+    this.patternElement,
+    this.bodyElementOption,
+    this.strokeElementOption,
+    this.patternElementOption,
+  );
 
   List<WebPolyline> get allElement {
-    final elements = <WebPolyline>[];
-    if (bodyElement != null) elements.add(bodyElement!);
+    final elements = <WebPolyline>[bodyElement];
     if (patternElement != null) elements.add(patternElement!);
     if (strokeElement != null) elements.add(strokeElement!);
     return elements;
