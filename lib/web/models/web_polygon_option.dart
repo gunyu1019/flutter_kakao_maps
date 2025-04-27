6e964ce42a5f8f306fa993b7da385c7c7fdc1767
gunyu1019 <gunyu1019@gmail.com>
2025-04-27 07:05:53 +0000

    [Feat] #13 - Implement external field to modify data in WebPolygonOption and WebPolylineOption.

--- lib/web/models/web_polygon_option.dart
+++ lib/web/models/web_polygon_option.dart
@@ -2,7 +2,7 @@ part of '../../kakao_map_sdk.dart';
 
 extension type WebPolygonOption._(JSObject _) implements JSObject {
   external WebPolygonOption(
-      {List<List<WebLatLng>> path,
+      {JSArray<JSArray<WebLatLng>> path,
       String fillColor,
       double fillOpacity = 0,
       String strokeColor,
@@ -10,4 +10,22 @@ extension type WebPolygonOption._(JSObject _) implements JSObject {
       double strokeOpacity = 0.6,
       String strokeStyle = "solid",
       int zIndex = 10000});
+
+  external JSArray<JSArray<WebLatLng>> get path;
+  external String get fillColor;
+  external double get fillOpacity;
+  external String get strokeColor;
+  external double get strokeWeight;
+  external double get strokeOpacity;
+  external String get strokeStyle;
+  external int get zIndex;
+
+  external set path(JSArray<JSArray<WebLatLng>> value);
+  external set fillColor(String value);
+  external set fillOpacity(double value);
+  external set strokeColor(String value);
+  external set strokeWeight(double value);
+  external set strokeOpacity(double value);
+  external set strokeStyle(String value);
+  external set zIndex(int value);
 }
