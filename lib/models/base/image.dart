be5cc5e19ffc42fe5e746ddaf8303f0f09e014b0
gunyu1019 <gunyu1019@gmail.com>
2025-04-30 09:53:14 +0000

    [Feat] Add fromWidget factory method.

--- lib/models/base/image.dart
+++ lib/models/base/image.dart
@@ -1,6 +1,7 @@
 part of '../../kakao_map_sdk.dart';
 
 /// 지도에서 사용할 수 있는 이미지를 생성하는 객체입니다.
+/// 생성된 이미지 객체는 [Poi] 또는 [RouteStyle.withPattern] 등의 이미지가 필요한 기능에서 사용할 수 있습니다.
 class KImage {
   final String? _path;
   final Uint8List? _data;
@@ -32,6 +33,10 @@ class KImage {
   /// 이미지 파일로 이미지 객체를 생성합니다.
   factory KImage.fromFile(File file, int width, int height) =>
       KImage._(ImageType.file, path: file.path, width: width, height: height);
+
+  /// Widget을 이미지로 만들어 사용합니다. 
+  /// 위젯을 이미지로 만드는 것이기 때문에, 버튼 등의 상호작용 기능은 작용하지 않습니다.
+  factory KImage.fromWidget(Widget child) => throw UnimplementedError();
 
   Map<String, dynamic> toMessageable() {
     final payload = <String, dynamic>{
