71e8b7492fc52a99c13ba44601c92d2d9320a6ce
gunyu1019 <gunyu1019@gmail.com>
2025-05-05 05:38:43 +0000

    [Fix] Separate with web plugin and native plugin.

--- lib/widget/map_widget.dart
+++ lib/widget/map_widget.dart
@@ -104,22 +104,6 @@ class _KakaoMapState extends State<KakaoMap> with KakaoMapControllerHandler {
   }
 
   void onPlatformViewCreated(int viewId) {
-    /* if (kIsWeb) {
-      final webMapOption =
-          WebMapOption.fromMapOption(widget.option ?? const KakaoMapOption());
-      WebInitializer.getController(viewId, webMapOption).then((webController) {
-        if (webController == null) {
-          final error = KakaoMapError("KAKAO_MAP_WEB_LOAD_FAILED",
-              "Timeout loading map elements. Please retry later.");
-          onMapError(error);
-          return;
-        }
-        controller =
-            KakaoMapWebController(controller: webController, handler: this);
-        onMapReady();
-      });
-      return;
-    } */
     channel = ChannelType.view.channelWithId(viewId);
     channel.setMethodCallHandler(handle);
 
@@ -140,10 +124,6 @@ class _KakaoMapState extends State<KakaoMap> with KakaoMapControllerHandler {
     if (widget.onTerrainLongClick != null) {
       bitMask |= EventType.onTerrainLongClick.id;
     }
-    /* if (kIsWeb) {
-      (controller as KakaoMapWebController)._setEventTrigger(bitMask);
-      return;
-    } */
     channel.invokeMethod("setEventHandler", bitMask);
   }
 
