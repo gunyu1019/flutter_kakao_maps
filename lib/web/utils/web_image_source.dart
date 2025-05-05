a624a122c366ed2735c2bb709521a9bf515ab912
gunyu1019 <gunyu1019@gmail.com>
2025-05-05 05:39:45 +0000

    [Fix] Independences KImage model in convertImageToData function.

--- lib/web/utils/web_image_source.dart
+++ lib/web/utils/web_image_source.dart
@@ -1,11 +1,11 @@
 part of '../kakao_map_sdk_web.dart';
 
-Future<Uint8List> convertImageToData(KImage image) async =>
-    switch (image.type) {
+Future<Uint8List> convertImageToData(dynamic image) async =>
+    switch (ImageType.values.where((e) => e.value == image["type"]).first) {
       ImageType.assets =>
-        (await rootBundle.load(image._path!)).buffer.asUint8List(),
-      ImageType.file => await File(image._path!).readAsBytes(),
-      ImageType.data => image._data!,
+        (await rootBundle.load(image["path"])).buffer.asUint8List(),
+      ImageType.file => await File(image["path"]).readAsBytes(),
+      ImageType.data => image["data"],
     };
 
 String encodeImageToBase64(Uint8List image, [String imageType = "png"]) =>
