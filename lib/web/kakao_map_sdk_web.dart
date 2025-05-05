5e854812b1a6f3b4404a19893319bfe71f50dbeb
gunyu1019 <gunyu1019@gmail.com>
2025-05-05 05:41:23 +0000

    [Feat] Make a WebInitializer to KakaoMapWebPlugin class.

--- lib/web/kakao_map_sdk_web.dart
+++ lib/web/kakao_map_sdk_web.dart
@@ -4,14 +4,14 @@ import 'dart:js_interop';
 import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/services.dart';
-import 'package:kakao_map_sdk/kakao_map_sdk.dart';
 import 'package:uuid/uuid.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:web/web.dart' as web;
+import 'package:kakao_map_sdk/kakao_map_sdk.dart';
 
 /* web (Experimentation) */
-part 'web_controller.dart';
-part 'web_initializer.dart';
+part 'controller/web_controller.dart';
+part 'controller/web_controller_handler.dart';
 
 part 'elements/image_element.dart';
 part 'elements/poi_element.dart';
@@ -24,10 +24,10 @@ part 'models/web_polygon_option.dart';
 part 'models/web_polyline_option.dart';
 part 'models/web_route.dart';
 
-part 'overlay/web_label_controller.dart';
+/* part 'overlay/web_label_controller.dart';
 part 'overlay/web_lod_label_controller.dart';
 part 'overlay/web_route_controller.dart';
-part 'overlay/web_shape_controller.dart';
+part 'overlay/web_shape_controller.dart'; */
 
 part 'interoperability/web_abstract_overlay.dart';
 part 'interoperability/web_custom_overlay.dart';
@@ -43,3 +43,58 @@ part 'interoperability/web_polyline.dart';
 part 'utils/web_calculate_level.dart';
 part 'utils/web_image_source.dart';
 part 'utils/web_color.dart';
+
+class KakaoMapWebPlugin {
+  // ignore: constant_identifier_names
+  static const VIEW_TYPE = "plugin/kakao_map";
+
+  static const int maxAttempts = 100;
+  static const int retryTime = 1;
+
+  static const codec = StandardMethodCodec();
+  static Registrar? registrar;
+
+  static String mapElementId(int viewId) => "map_$viewId";
+
+  static web.Element viewFactory(int viewId, {Object? params}) {
+    final channel =
+        ChannelType.view.channelWithParamAndId(viewId, codec, registrar);
+    final overlayChannel =
+        ChannelType.view.channelWithParamAndId(viewId, codec, registrar);
+    final webMapOption = WebMapOption.fromMessageable(params!);
+
+    getController(viewId, webMapOption).then((webController) {
+      KakaoMapWebController(
+          controller: webController,
+          channel: channel,
+          overlayChannel: overlayChannel);
+    });
+
+    return web.HTMLDivElement()
+      ..id = mapElementId(viewId)
+      ..style.width = '100%'
+      ..style.height = '100%';
+  }
+
+  static void registerWith(Registrar registrar) {
+    KakaoMapWebPlugin.registrar = registrar;
+    ui_web.platformViewRegistry
+        .registerViewFactory("plugin/kakao_map", viewFactory);
+    
+    // Unused in Web Environment
+    final sdkChannel = ChannelType.sdk.channelWithParam(codec, registrar);
+    sdkChannel.setMethodCallHandler((handler) async {});
+  }
+
+  static Future<WebMapController?> getController(
+      int viewId, WebMapOption option) async {
+    for (int i = 0; i < maxAttempts; i++) {
+      var element = web.document.getElementById("map_$viewId");
+      if (element != null) {
+        return WebMapController(element, option);
+      }
+      await Future.delayed(const Duration(seconds: retryTime));
+    }
+    return null;
+  }
+}
