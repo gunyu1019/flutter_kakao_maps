de2552aff7f1c15bd74ac2a7a114224b24e82bae
gunyu1019 <gunyu1019@gmail.com>
2025-05-06 08:14:10 +0000

    [Fix] #18 - Implement RoutePattern.fromMessageable construction.

--- lib/models/route/route_pattern.dart
+++ lib/models/route/route_pattern.dart
@@ -35,4 +35,13 @@ class RoutePattern with KMessageable {
       "pinEnd": pinEnd
     };
   }
+  
+  factory RoutePattern.fromMessageable(dynamic payload) =>
+    RoutePattern(
+      KImage.fromMessageable(payload["patternImage"]),
+      payload["distance"],
+      symbolImage: payload["symbolImage"] != null ? KImage.fromMessageable(payload["symbolImage"]) : null,
+      pinStart: payload["pinStart"],
+      pinEnd: payload["pinEnd"]
+    );
 }
