bc5de6ccc42e47616db2ff7be69c73f64213da8c
gunyu1019 <gunyu1019@gmail.com>
2025-05-06 08:16:19 +0000

    [Fix] #18 - Seperate web route controller from dart-side.

--- lib/web/overlay/web_route_controller.dart
+++ lib/web/overlay/web_route_controller.dart
@@ -1,22 +1,30 @@
 part of '../kakao_map_sdk_web.dart';
 
-class WebRouteController extends RouteController {
+class WebRouteController with WebRouteControllerHandler {
+  final String id;
   final WebMapController controller;
 
   final Map<String, List<WebRoute>> _webRoute = {};
   final Map<String, List<int>> _currentRouteLevel = {};
 
-  WebRouteController._(this.controller, super.channel, super.manager, super.id)
-      : super._();
+  @override
+  final Map<String, String> _routeStyleId = {};
+  final Map<String, List<int>> _routeStyleIndex = {};
+  final Map<String, int> _routeZOrder = {};
 
   @override
-  Future<void> _createRouteLayer() async {
+  final WebOverlayController manager;
+
+  WebRouteController._(this.id, this.controller, this.manager);
+
+  @override
+  Future<void> createRouteLayer() async {
     addEventListener(controller, "zoom_changed", _zoomChangedEventHandler.toJS);
   }
 
   @override
-  Future<void> _removeRouteLayer() async {
-    for (final route in _route.values) {
+  Future<void> removeRouteLayer() async {
+    for (final route in _webRoute.keys) {
       await removeRoute(route);
     }
     removeEventListener(
@@ -24,23 +32,21 @@ class WebRouteController extends RouteController {
   }
 
   void _zoomChangedEventHandler() {
-    for (var route in _route.values) {
-      _syncZoomLevel(
-          route.id,
-          route.multiple
-              ? (route as MultipleRoute).styles
-              : [(route as Route).style]);
+    for (final route in _webRoute.keys) {
+      final styles = manager._routeStyles[_routeStyleId[route]!]!;
+      _syncZoomLevel(route, styles);
     }
   }
 
   void _syncZoomLevel(String routeId, List<RouteStyle> styles) {
     final mapZoomLevel = controller.getLevel();
     final webRoute = _webRoute[routeId]!;
+    final zOrder = _routeZOrder[routeId]!;
 
     var currentStyles = styles;
     var currentZoomLevel = styles.map((e) => e.zoomLevel).toList();
     for (final (index, style) in styles.indexed) {
-      for (final secondaryStyle in style._styles) {
+      for (final secondaryStyle in style.otherStyles) {
         if (_calculateZoomLevel(secondaryStyle.zoomLevel) >= mapZoomLevel &&
             secondaryStyle.zoomLevel >= currentZoomLevel[index]) {
           currentZoomLevel[index] = secondaryStyle.zoomLevel;
@@ -95,37 +101,35 @@ class WebRouteController extends RouteController {
   }
 
   @override
-  Future<void> _changeMultipleRoute(
-      String routeId, String styleId, List<RouteSegment> segments) async {
-    final zOrder = _route[routeId]!.zOrder;
-    final style = manager.getMultipleRotueStyle(styleId)!;
+  Future<void> changeRoute(
+      String routeId, String styleId, List<List<LatLng>> points) async {
+    final webRoute = _webRoute[routeId]!;
+    final zOrder = _routeZOrder[routeId]!;
+    final styleIndex = _routeStyleIndex[routeId]!;
+    final styles = manager._routeStyles[styleId]!;
 
     for (final webRoute in _webRoute[routeId]!) {
       for (final routeElement in webRoute.allElement) {
         routeElement.setMap(null);
       }
     }
-    _webRoute[routeId] = segments
-        .map((segment) =>
-            _addRouteElement(style[segment.styleIndex], segment.points, zOrder))
+    _webRoute[routeId] = points
+        .mapIndexed((index, point) => _addRouteElement(
+            styles[styleIndex.elementAtOrNull(index) ?? 0], point, zOrder))
         .toList();
   }
 
   @override
-  Future<void> _changeRoute(String routeId, String styleId, CurveType curveType,
-      List<LatLng> points) async {
-    final webRoute = _webRoute[routeId]![0];
-    final zOrder = _route[routeId]!.zOrder;
-    final style = manager.getRotueStyle(styleId)!;
-
-    for (final routeElement in webRoute.allElement) {
-      routeElement.setMap(null);
+  Future<void> changeRouteZOrder(String routeId, int zOrder) async {
+    for (final webRoute in _webRoute[routeId]!) {
+      webRoute.bodyElement.setZIndex(zOrder);
+      webRoute.strokeElement?.setZIndex(zOrder - 1);
+      webRoute.patternElement?.setZIndex(zOrder + 1);
     }
-    _webRoute[routeId] = [_addRouteElement(style, points, zOrder)];
   }
 
   @override
-  Future<void> _changeRouteVisible(String routeId, bool visible) async {
+  Future<void> changeRouteVisible(String routeId, bool visible) async {
     for (final route in _webRoute[routeId]!) {
       if (visible) {
         route.bodyElement.setMap(controller);
@@ -192,70 +196,56 @@ class WebRouteController extends RouteController {
   }
 
   @override
-  Future<Route> addRoute(List<LatLng> points, RouteStyle style,
+  Future<String> addRoute(List<LatLng> points, RouteStyle style,
       {String? id,
       CurveType curveType = CurveType.none,
       int zOrder = 10000}) async {
-    if (id != null && _route.containsKey(id)) {
-      throw DuplicatedOverlayException(id);
-    }
-    style.id ?? await manager.addRouteStyle(style);
-
-    String routeId = "route_overlay_${id}_${_route.length}";
+    String routeId = "route_overlay_${this.id}_${_webRoute.length}";
     _webRoute[routeId] = [_addRouteElement(style, points, zOrder)];
+    _routeStyleId[routeId] = style.id!;
+    _routeZOrder[routeId] = zOrder;
+    _routeStyleIndex[routeId] = [0];
 
-    final route = Route._(this, routeId,
-        points: points, style: style, curveType: curveType, zOrder: zOrder);
-    _route[routeId] = route;
     _currentRouteLevel[routeId] = [style.zoomLevel];
     _syncZoomLevel(routeId, [style]);
-    return route;
+    return routeId;
   }
 
   @override
-  Future<MultipleRoute> addMultipleRoute(MultipleRouteOption option) async {
-    if (option.id != null && _route.containsKey(option.id)) {
-      throw DuplicatedOverlayException(option.id!);
-    }
-    if (!option._isStyleAdded()) {
-      await manager.addMultipleRouteStyle(option.styles);
-    }
-
-    String routeId = "multiple_route_overlay_${id}_${_route.length}";
+  Future<String> addMultipleRoute(MultipleRouteOption option) async {
+    String routeId = "multiple_route_overlay_${id}_${_webRoute.length}";
     _webRoute[routeId] = option.segments
         .map((segment) =>
             _addRouteElement(segment.style, segment.points, option.zOrder))
         .toList();
 
-    final route = MultipleRoute._(this, routeId,
-        segments: option.segments,
-        styles: option.styles,
-        zOrder: option.zOrder);
-    _route[routeId] = route;
     _currentRouteLevel[routeId] =
         option.styles.map((e) => e.zoomLevel).toList();
+    _routeStyleId[routeId] = option.styles[0].id!;
+    _routeZOrder[routeId] = option.zOrder;
+    _routeStyleIndex[routeId] =
+        option.segments.map((e) => e.styleIndex).toList();
     _syncZoomLevel(routeId, option.styles);
-    return route;
+    return routeId;
   }
 
   @override
-  Future<void> removeRoute(BaseRoute route) async {
-    await _changeRouteVisible(route.id, false);
-    _route.remove(route.id);
-    _webRoute.remove(route.id);
+  Future<void> removeRoute(String routeId) async {
+    await changeRouteVisible(routeId, false);
+    _webRoute.remove(routeId);
   }
 
   @override
   Future<void> showAllRoute() async {
-    for (var route in _route.values) {
-      await route.show();
+    for (var route in _webRoute.keys) {
+      await changeRouteVisible(route, true);
     }
   }
 
   @override
   Future<void> hideAllRoute() async {
-    for (var route in _route.values) {
-      await route.hide();
+    for (var route in _webRoute.keys) {
+      await changeRouteVisible(route, false);
     }
   }
 }
