e609a04f67b48624b5d49dbdf616eb79ffc572bc
gunyu1019 <gunyu1019@gmail.com>
2025-05-07 10:37:19 +0000

    [Fix] #19 - Remove width, height size adjusting.

--- lib/web/elements/image_element.dart
+++ lib/web/elements/image_element.dart
@@ -3,7 +3,7 @@ part of '../kakao_map_sdk_web.dart';
 web.HTMLElement imageElement(String source, int width, int height,
         [void Function()? onClick]) =>
     web.HTMLImageElement()
-      ..width = width * 3
-      ..height = height * 3
+      ..width = width
+      ..height = height
       ..onclick = onClick?.toJS
       ..src = source;
