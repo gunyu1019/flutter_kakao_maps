2bfd0774efc8e7b7997b9c18d7855a576ee1ce4d
gunyu1019 <gunyu1019@gmail.com>
2025-06-03 03:43:03 -0700

    [Revert] Insert test code.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -87,14 +87,9 @@ class _KakaoMapViewState extends State<KakaoMapView> {
             title: "Shape",
             textStyle: controllerTextStyle,
             onChanged: (value) {
-              if (value) {
-                // controller.pause();
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height,
-                        color: Colors.green)));
-              }
+              value
+                  ? controller.shapeLayer.showAllPolyline()
+                  : controller.shapeLayer.hideAllPolyline();
               setState(() => shapeVisible = value);
             }),
         SwitchComponent(
