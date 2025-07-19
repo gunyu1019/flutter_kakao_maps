fa2b4f3a17a58dcb1e05d00647af8c6d19844d71
gunyu1019 <gunyu1019@gmail.com>
2025-07-19 08:05:25 +0000

    [Formatting] Re-formatting with swiftformat.

--- ios/kakao_map_sdk/Package.swift
+++ ios/kakao_map_sdk/Package.swift
@@ -3,25 +3,25 @@ import PackageDescription
 let package = Package(
     name: "kakao_map_sdk",
     platforms: [
-        .iOS("13.0")
+        .iOS("13.0"),
     ],
     products: [
-        .library(name: "kakao-map-sdk", targets: ["kakao_map_sdk"])
+        .library(name: "kakao-map-sdk", targets: ["kakao_map_sdk"]),
     ],
     dependencies: [
         .package(
-        url: "https://github.com/kakao-mapsSDK/KakaoMapsSDK-SPM.git",
-        from: "2.12.5"
-        )
+            url: "https://github.com/kakao-mapsSDK/KakaoMapsSDK-SPM.git",
+            from: "2.12.5"
+        ),
     ],
     targets: [
         .target(
             // TODO: Update your target name.
             name: "kakao_map_sdk",
             dependencies: [
-                .product(name: "KakaoMapsSDK-SPM", package: "KakaoMapsSDK-SPM")
+                .product(name: "KakaoMapsSDK-SPM", package: "KakaoMapsSDK-SPM"),
             ],
             resources: []
-        )
+        ),
     ]
 )
