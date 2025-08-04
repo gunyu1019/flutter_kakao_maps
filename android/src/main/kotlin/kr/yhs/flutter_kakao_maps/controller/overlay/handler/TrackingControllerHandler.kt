f1821bc8f3227c9afbce440af72e59090c249b5a
gunyu1019 <gunyu1019@gmail.com>
2025-08-04 07:12:59 +0000

    [Feat] #32 - Implement Tracking Controller handler.

---
+++ android/src/main/kotlin/kr/yhs/flutter_kakao_maps/controller/overlay/handler/TrackingControllerHandler.kt
@@ -0,0 +1,41 @@
+package kr.yhs.flutter_kakao_maps.controller.overlay.handler
+
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import java.util.Arrays
+import kr.yhs.flutter_kakao_maps.converter.PrimitiveTypeConverter.asBoolean
+import kr.yhs.flutter_kakao_maps.converter.PrimitiveTypeConverter.asInt
+import kr.yhs.flutter_kakao_maps.converter.PrimitiveTypeConverter.asMap
+import kr.yhs.flutter_kakao_maps.converter.PrimitiveTypeConverter.asString
+import kr.yhs.flutter_kakao_maps.converter.ShapeTypeConverter.asDotPoints
+import kr.yhs.flutter_kakao_maps.converter.ShapeTypeConverter.asMapPoints
+import kr.yhs.flutter_kakao_maps.converter.ShapeTypeConverter.asPolygonOption
+import kr.yhs.flutter_kakao_maps.converter.ShapeTypeConverter.asPolygonStylesSet
+import kr.yhs.flutter_kakao_maps.converter.ShapeTypeConverter.asPolylineOption
+import kr.yhs.flutter_kakao_maps.converter.ShapeTypeConverter.asPolylineStylesSet
+import kr.yhs.flutter_kakao_maps.converter.ShapeTypeConverter.asShapeLayerOption
+import com.kakao.vectormap.label.TrackingManager
+import com.kakao.vectormap.label.Label
+import com.kakao.vectormap.label.LabelManager
+
+interface TrackingControllerHandler {
+  val trackingManager: TrackingManager?
+
+  val labelManager: LabelManager?
+ 
+  fun tackingHandle(call: MethodCall, result: MethodChannel.Result) {
+    val arguments = call.arguments!!.asMap<Any?>()
+    if (trackingManager == null) {
+      throw NullPointerException("TrackingManager is null.")
+    }
+
+    when (call.method) {
+    }
+  }
+  
+  fun setTrackkingRotation(rotation: Boolean, onSuccess: (Any?) -> Unit)
+
+  fun startTracking(label: Label, onSuccess: (Any?) -> Unit)
+
+  fun stopTracking(onSuccess: (Any?) -> Unit)
+}
