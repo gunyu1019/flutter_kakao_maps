4efb85b97b7d9e47b7673a00158902bf4861a208
gunyu1019 <gunyu1019@gmail.com>
2026-07-03 22:52:01 +0900

    [Feat] Implement WebDimScreenControllerHandler mixin for handling DimScreen operations

---
+++ lib/web/overlay/web_dim_screen_controller_handler.dart
@@ -0,0 +1,68 @@
+part of '../kakao_map_sdk_web.dart';
+
+mixin WebDimScreenControllerHandler {
+  WebOverlayController get manager;
+
+  Future<dynamic> dimScreenHandle(MethodCall method) async {
+    final arguments = method.arguments;
+
+    switch (method.method) {
+      case "createDimScreenLayer":
+        await createDimScreenLayer();
+        return;
+      case "removeDimScreenLayer":
+        await removeDimScreenLayer();
+        return;
+      case "setColor":
+        await setDimColor(arguments["color"]);
+        return;
+      case "setVisible":
+        await setDimVisible(arguments["visible"]);
+        return;
+      case "setDimCover":
+        final cover = DimScreenCover.values.firstWhere(
+          (e) => e.value == arguments["cover"],
+        );
+        await setDimCover(cover);
+        return;
+      case "addHighlightPolygonShape":
+        final polygon = arguments["polygon"];
+        final point = WebShapePoint.fromMessageable(polygon["position"]);
+        return await addHighlightPolygonShape(point, id: polygon["id"]);
+      case "removeHighlightPolygonShape":
+        final shapeId = arguments["polygonId"];
+        await removeHighlightPolygonShape(shapeId);
+        return;
+      case "changePolygonVisible":
+        final shapeId = arguments["polygonId"];
+        final visible = arguments["visible"];
+        await changePolygonVisible(shapeId, visible);
+        return;
+      case "changePolygon":
+        final shapeId = arguments["polygonId"];
+        final point = WebShapePoint.fromMessageable(arguments["position"]);
+        await changePolygon(shapeId, point);
+        return;
+      default:
+        throw UnimplementedError();
+    }
+  }
+
+  Future<void> createDimScreenLayer();
+
+  Future<void> removeDimScreenLayer();
+
+  Future<void> setDimColor(int color);
+
+  Future<void> setDimVisible(bool visible);
+
+  Future<void> setDimCover(DimScreenCover cover);
+
+  Future<String> addHighlightPolygonShape(WebShapePoint point, {String? id});
+
+  Future<void> removeHighlightPolygonShape(String shapeId);
+
+  Future<void> changePolygonVisible(String shapeId, bool visible);
+
+  Future<void> changePolygon(String shapeId, WebShapePoint point);
+}
