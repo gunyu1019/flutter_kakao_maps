91eca54b34826c9999a1387a88b377962fa41bf9
gunyu1019 <gunyu1019@gmail.com>
2026-07-04 00:39:22 +0900

    [Feat] Add WebDimHighlightShape class for managing highlight shapes in DimScreen

---
+++ lib/web/models/web_dim_highlight_shape.dart
@@ -0,0 +1,11 @@
+part of '../kakao_map_sdk_web.dart';
+
+
+class WebDimHighlightShape {
+  final String id;
+
+  WebShapePoint point;
+  bool visible = true;
+
+  WebDimHighlightShape(this.point, this.id);
+}
