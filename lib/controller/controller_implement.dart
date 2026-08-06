85818a98ed46a8f9d743406cf697b8b6d2bd79d9
gunyu1019 <gunyu1019@gmail.com>
2026-08-06 21:54:11 +0900

    [Feat] Integrate getBounds method in controller.

--- lib/controller/controller_implement.dart
+++ lib/controller/controller_implement.dart
@@ -498,4 +498,25 @@ class KakaoMapControllerImplement extends KakaoMapController {
     buildingHeightScale = null;
     _initalizeOverlayController();
   }
+
+  @override
+  Future<LatLngBounds?> getBounds(BuildContext context) async {
+    /// 지도 위젯을 포함하는 RenderBox 객체입니다
+    final renderBox = context.findRenderObject();
+
+    if (renderBox is! RenderBox) {
+      return null;
+    }
+
+    final size = renderBox.size;
+    final [ne, sw] = await Future.wait([
+      fromScreenPoint(size.width.toInt(), 0),
+      fromScreenPoint(0, size.height.toInt())
+    ]);
+
+    if (ne == null || sw == null) {
+      return null;
+    }
+    return LatLngBounds._(ne, sw);
+  }
 }
