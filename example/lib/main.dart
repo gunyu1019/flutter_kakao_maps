20833b3abafef54a7ef617ef26b04ee2e45f42d2
gunyu1019 <gunyu1019@gmail.com>
2026-09-07 04:32:23 +0900

    [Fix] Update example app to include polygon shapes and map click events

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,6 +43,14 @@ class _KakaoMapViewState extends State<KakaoMapView> {
   late bool shapeVisible;
   late bool routeVisible;
   late bool dimScreenVisible;
+  late bool polylineTextVisible;
+  late bool eventEnable;
+
+  final polylineTextPoints = [
+    const LatLng(37.39622327123534, 127.10969230372446),
+    const LatLng(37.395410161239674, 127.11202881608124),
+    const LatLng(37.39380557163993, 127.1128395227644)
+  ];
 
   final location = <LocationInfo>[
     LocationInfo(
@@ -91,6 +99,9 @@ class _KakaoMapViewState extends State<KakaoMapView> {
             value
                 ? controller.shapeLayer.showAllPolyline()
                 : controller.shapeLayer.hideAllPolyline();
+            value
+                ? controller.shapeLayer.showAllPolygon()
+                : controller.shapeLayer.hideAllPolygon();
             setState(() => shapeVisible = value);
           },
         ),
@@ -110,6 +121,23 @@ class _KakaoMapViewState extends State<KakaoMapView> {
           onChanged: (value) {
             controller.dimScreen.setVisible(value);
             setState(() => dimScreenVisible = value);
+          },
+        ),
+        /* SwitchComponent(
+          title: "PolylineText",
+          textStyle: controllerTextStyle,
+          onChanged: (value) {
+            value
+                ? controller.labelLayer.showAllPolylineText()
+                : controller.labelLayer.hideAllPolylineText();
+            setState(() => polylineTextVisible = value);
+          },
+        ), */
+        SwitchComponent(
+          title: "Event",
+          textStyle: controllerTextStyle,
+          onChanged: (value) {
+            setState(() => eventEnable = value);
           },
         ),
       ],
@@ -140,6 +168,7 @@ class _KakaoMapViewState extends State<KakaoMapView> {
   // 지도 뷰
   Widget mapWidget(BuildContext context) => KakaoMap(
     onMapReady: onMapReady,
+    onMapClick: onMapClick,
     option: const KakaoMapOption(position: LatLng(37.394776, 127.11116)),
   );
 
@@ -148,7 +177,9 @@ class _KakaoMapViewState extends State<KakaoMapView> {
     poiVisible = false;
     shapeVisible = false;
     routeVisible = false;
-    dimScreenVisible = true;
+    dimScreenVisible = false;
+    polylineTextVisible = false;
+    eventEnable = false;
     super.initState();
   }
 
@@ -157,7 +188,7 @@ class _KakaoMapViewState extends State<KakaoMapView> {
     body: DrawerComponent(
       body: mapWidget(context),
       drawer: controllerWidget(),
-      maxHeight: 260,
+      maxHeight: 320,
       minHeight: 60,
     ),
   );
@@ -172,10 +203,20 @@ class _KakaoMapViewState extends State<KakaoMapView> {
   // 예제에 구현할 오버레이를 지도에 등록합니다.
   Future<void> initializeOverlay() async {
     var poiStyle = PoiStyle(
+      textStyle: [const PoiTextStyle(
+        color: Colors.red,
+        stroke: 16,
+        strokeColor: Colors.white,
+        size: 48,
+      )],
       icon: KImage.fromAsset("assets/image/location.png", 40, 60),
     );
     for (var loc in location) {
-      await controller.labelLayer.addPoi(loc.position, style: poiStyle);
+      await controller.labelLayer.addPoi(
+          loc.position,
+          style: poiStyle,
+          text: loc.name
+      );
     }
 
     // /assets/const/shape.json 에 사전에 등록한 도형를 불러옵니다.
@@ -184,17 +225,34 @@ class _KakaoMapViewState extends State<KakaoMapView> {
     );
     List<dynamic> shapePoints = json.decode(shapeRawData);
 
-    var polylineStyle = PolylineStyle(Colors.deepOrange, 12);
+    var polylineStyle1 = PolylineStyle(Colors.deepOrange, 12);
+    var polylineStyle2 = PolygonStyle(Colors.green);
+    var polylineStyle3 = PolygonStyle(
+      Colors.yellow.withValues(alpha: 0.3),
+      strokeColor: Colors.blue,
+      strokeWidth: 3,
+    );
+    await controller.addPolygonShapeStyle(polylineStyle2);
+    await controller.addPolygonShapeStyle(polylineStyle3);
+
     for (var rawPoint in shapePoints) {
       var point = List<dynamic>.from(
         rawPoint,
       ).map((e) => List<double>.from(e)).toList();
       await controller.shapeLayer.addPolylineShape(
         MapPoint(point.map((e) => LatLng(e[0], e[1])).toList()),
-        polylineStyle,
+        polylineStyle1,
         PolylineCap.round,
       );
     }
+    await controller.shapeLayer.addPolygonShape(
+      CirclePoint(400, const LatLng(37.39922517606363,127.10805907837934)),
+      polylineStyle2,
+    );
+    await controller.shapeLayer.addPolygonShape(
+      RectanglePoint(340, 400, const LatLng(37.39990534855002, 127.11298419463544)),
+      polylineStyle3,
+    );
 
     // /assets/const/route.json 에 사전에 등록한 경로를 불러옵니다.
     final String routeRawData = await rootBundle.loadString(
@@ -213,33 +271,70 @@ class _KakaoMapViewState extends State<KakaoMapView> {
       routeStyle,
     );
 
+    /* final polylineTextStyle = PolylineTextStyle(
+        64, Colors.blue,
+        strokeColor: Colors.white,
+        strokeSize: 3
+    );
+
+    await controller.labelLayer.addPolylineText(
+      "Polyline Text (휘어진 글씨)",
+      polylineTextPoints,
+      style: polylineTextStyle,
+    ); */
+
     // 카카오 판교캠퍼스 주변을 사각형으로 강조하는 DimScreen을 구성합니다.
     await controller.dimScreen.setColor(Colors.black.withValues(alpha: 0.6));
     // Highlight 영역에는 지도도 보이면서 스타일도 함께 표시됩니다.
-    final highlightStyle = PolygonStyle(
+    final highlightStyle1 = PolygonStyle(
       Colors.lightBlueAccent.withValues(alpha: 0.25),
       strokeColor: Colors.yellowAccent,
       strokeWidth: 6,
     );
-    await controller.addPolygonShapeStyle(highlightStyle);
+    final highlightStyle2 = PolygonStyle(Colors.orange);
+    final highlightStyle3 = PolygonStyle(
+      Colors.green.withValues(alpha: 0.3),
+      strokeColor: Colors.red,
+      strokeWidth: 3,
+    );
+    await controller.addPolygonShapeStyle(highlightStyle1);
+    await controller.addPolygonShapeStyle(highlightStyle2);
+    await controller.addPolygonShapeStyle(highlightStyle3);
+
     await controller.dimScreen.addPolygonShape(
       MapPoint([
         const LatLng(37.393, 127.109),
         const LatLng(37.393, 127.113),
         const LatLng(37.397, 127.113),
         const LatLng(37.397, 127.109),
+        const LatLng(37.393, 127.109),
       ]),
-      highlightStyle,
+      highlightStyle1,
     );
+    await controller.dimScreen.addPolygonShape(
+      CirclePoint(400, const LatLng(37.39922517606363,127.10805907837934)),
+      highlightStyle2,
+    );
+    await controller.dimScreen.addPolygonShape(
+      RectanglePoint(340, 400, const LatLng(37.39990534855002, 127.11298419463544)),
+      highlightStyle3,
+    );
+
     poiVisible
         ? await controller.labelLayer.showAllPoi()
         : await controller.labelLayer.hideAllPoi();
     shapeVisible
         ? await controller.shapeLayer.showAllPolyline()
         : await controller.shapeLayer.hideAllPolyline();
+    shapeVisible
+        ? await controller.shapeLayer.showAllPolygon()
+        : await controller.shapeLayer.hideAllPolygon();
     routeVisible
         ? await controller.routeLayer.showAllRoute()
         : await controller.routeLayer.hideAllRoute();
+    /* polylineTextVisible
+        ? controller.labelLayer.showAllPolylineText()
+        : controller.labelLayer.hideAllPolylineText(); */
     await controller.dimScreen.setVisible(dimScreenVisible);
   }
 
@@ -247,5 +342,19 @@ class _KakaoMapViewState extends State<KakaoMapView> {
   void onMapReady(KakaoMapController controller) {
     this.controller = controller;
     initializeOverlay();
+  }
+
+  void onMapClick(KPoint point, LatLng latLng) {
+    if (!eventEnable) {
+      return;
+    }
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("${latLng.latitude}, ${latLng.longitude}"),
+      )
+    );
+    Clipboard.setData(
+        ClipboardData(text: "${latLng.latitude}, ${latLng.longitude}")
+    );
   }
 }
